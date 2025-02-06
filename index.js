e3609516951b56cf50628b8d6bfa91cfec97252a
Ismail <ayupovismail07@gmail.com>
2025-02-06 11:45:00 +0500

    Second version fix 7 Add robots.txt

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow:");
+});
+
 app.post('/request', async (req, res) => {
     try {
         const { name, email, phone } = req.body;
